4a3a9142b0d18a958621b28c2d65cc2bbf74131a
Domagoj Majcenić (dmajcenic) <dmajcenic@tvz.hr>
2017-07-02 16:38:50 +0200

    actually finished nod-web-server chapter

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -43,6 +43,14 @@ app.get('/', (req, res) => {
     });
 });
 
+app.get('/projects', (req, res) => {
+    //res.send('<h1>Hello express!</h1>');
+    res.render('projects.hbs', {
+        pageTitle: 'Projects',
+        welcomeMessage: 'Welcome to the projects page'
+    });
+});
+
 app.get('/about', (req, res) => {
     res.render('about.hbs', {
         pageTitle: 'About Page',
